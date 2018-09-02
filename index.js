1d5f495aa5bc382670243ebeb60276d19b7a5f06
Manju Thomas <kudukacar@hotmail.com>
2018-09-02 12:12:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function currentLine(katzDeliLine) {
     if (katzDeliLine.length === 0) {
       return "The line is currently empty.";}
       else{
+      var line[];
       for (let i = 0; i < katzDeliLine.length; i++) {
         line.push(' ' + [i+1] + '.' + katzDeliLine[i]);
       }
