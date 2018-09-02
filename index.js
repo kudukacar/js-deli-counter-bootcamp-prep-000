99c70c92f2ea3e48d05abd246e437e53062ab85e
Manju Thomas <kudukacar@hotmail.com>
2018-09-02 10:47:44 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  for (let i = 0; i < katzDeliLine.length; i++) {
+    if (katzDeliLine[i]===name) {
+      var number = Number(i) + 1;
+      return "Welcome, " + name + "." + " You are number " + number + " in line.";}
+  }
+}
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {return "Currently serving " +  katzDeliLine.shift() + ".";}
+  else {return "There is nobody waiting to be served!";}
+}
+function currentLine(katzDeliLine) {
+    if (katzDeliLine.length === 0) {
+      return "The line is currently empty.";}
+      else {
+        while (i < katzDeliLine.length) {
+          i++;
+          var number = Number(i) + 1;
+      return "The line is currently: " + number + ". " + katzDeliLine[i] + ".";
+        }
+  }
+}
